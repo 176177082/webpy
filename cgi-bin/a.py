abc8710e04798a2f94a0bdf0501553e0c4fbb466
w176177082 <176177082@qq.com>
2017-07-02 00:24:46 +0800

    create a b

---
+++ cgi-bin/a.py
@@ -0,0 +1,10 @@
+#!C:\Python27\ArcGIS10.2\python.exe  
+# -*- coding: UTF-8 -*-
+"""
+__author__ = 'changxiong'
+"""
+
+print "Content-Type: text/html"
+print "Hello World"
+print "Hello World"
+print "Hello World"
